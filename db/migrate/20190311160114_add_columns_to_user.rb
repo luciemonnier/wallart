da8b0515c7d88660e4b0b1dae33b30c2e1bd1bf3
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-11 17:51:54 +0100

    navbar et search

---
+++ db/migrate/20190311160114_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :photo, :string
+    add_column :users, :subscription_type, :integer
+  end
+end
