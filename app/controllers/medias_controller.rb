e2e62cb46fdff9c7f455b026ec67cbea01fbdb74
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-12 15:49:21 +0100

    hello

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,6 +1,11 @@
 class MediasController < ApplicationController
   def index
-    @medias = Media.all
+    @categories = ["vidéo", "cinémagraphe", "photo numérique", "algorithme", "illustration", "peinture numérique"]
+    if params[:query].present?
+      @medias = Media.search_by_title_category_and_description(params[:query])
+    else
+      @medias = Media.all
+    end
   end
 
   def show
