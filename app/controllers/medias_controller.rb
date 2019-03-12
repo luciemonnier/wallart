6262bc0d587ce996668c0eb2a986e50510cad180
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-12 12:05:51 +0100

    oups

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,15 +1,6 @@
 class MediasController < ApplicationController
   def index
-    if params[:query].present?
-      # @medias = Media.all.select do |media|
-      #   media.tags.include?(params[:query].downsize)
-      # end
-      Media.search_by_title_and_description(params[:query]).each do |media|
-        @medias << media
-      end
-    else
-      @medias = Media.all
-    end
+    @medias = Media.all
   end
 
   def show
