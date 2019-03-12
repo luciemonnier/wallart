d376cd5b5693cf9ca909f87cf79ae969b4c54854
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-12 10:03:34 +0100

    search qui marche

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -4,6 +4,11 @@ class MediasController < ApplicationController
       @medias = Media.all.select do |media|
         media.tag_list.include?(params[:query].downsize)
       end
+      Media.search_by_title_and_description(params[:query]).each do |media|
+        @medias << media
+      end
+    else
+      @medias = Media.all
     end
   end
 
--- app/models/media.rb
+++ app/models/media.rb
@@ -1,11 +1,13 @@
+require 'pg_search'
+
 class Media < ApplicationRecord
   belongs_to :artist
   acts_as_taggable
 
-  # include PgSearch
-  # pg_search_scope :search_by_title_and_description,
-  #   against: [:title, :description],
-  #   using: {
-  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  #   }
+  include PgSearch
+  pg_search_scope :search_by_title_and_description,
+    against: [:title, :description],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
