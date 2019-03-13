a6eb3df0de52650496231ef92816dbcd01345610
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-13 17:42:33 +0100

    search amazing

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -3,14 +3,14 @@ class MediasController < ApplicationController
     @categories = Category.all
     if params[:query].present?
       @medias = Media.search_by_title_category_and_description(params[:query])
+      @medias = @medias.to_a
+      Media.all.each do |media|
+        if media.tags.any? { |tag| tag.name.match(/#{params[:query].downcase}/) }
+          @medias << media
+        end
+      end
     else
       @medias = Media.all
-    end
-    @medias = @medias.to_a
-    Media.all.each do |media|
-      if media.tags.any? { |tag| tag.name.match(/#{params[:query].downcase}/) }
-        @medias << media
-      end
     end
     return @medias
   end
