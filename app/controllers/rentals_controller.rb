5d21d3fa253b9f373933f7b4a48858c24168a383
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-13 16:18:51 +0100

    cloudinary

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,11 +1,13 @@
 class RentalsController < ApplicationController
   def index
     @images = ['rental1.jpg', 'rental2.jpg', 'rental3.jpg', 'rental4.jpg', 'rental5.jpg', 'rental6.jpg', 'rental7.jpg', ]
-    @rentals = Rental.all
+    @active_rentals = Rental.where(active: true)
+    @former_rentals = Rental.where(active: false)
   end
 
   def create
     @rental = Rental.new(params_rental)
+    @rental.active = true
     if @rental.save
       redirect_to rentals_path
     end
