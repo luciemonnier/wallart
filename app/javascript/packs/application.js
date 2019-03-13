a488a73ee1c542dd1007170219b7bfaeab2d1921
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-13 15:52:56 +0100

    sorry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,11 @@ import { loadDynamicBannerText } from '../plugins/typed';
 import { slickCarousel } from '../plugins/carousel'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-slickCarousel();
+
+const carousel = document.querySelector('.category-row')
+if (carousel) {
+  slickCarousel();
+}
 
 const banner = document.getElementById('banner-typed-text');
 if (banner) {
