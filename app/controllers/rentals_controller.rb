8f5b4690df5cef1396f9a8056eb4ac7df170b0ce
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-13 15:11:11 +0100

    carousel qui marche ptet sur heroku

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,4 +3,17 @@ class RentalsController < ApplicationController
     @images = ['rental1.jpg', 'rental2.jpg', 'rental3.jpg', 'rental4.jpg', 'rental5.jpg', 'rental6.jpg', 'rental7.jpg', ]
     @rentals = Rental.all
   end
+
+  def create
+    @rental = Rental.new(params_rental)
+    if @rental.save
+      redirect_to rentals_path
+    end
+  end
+
+  private
+
+  def params_rental
+    params.require(:rental).permit(:media_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   get '/', to: 'pages#home', as: 'home'
-  resources 'medias', only: [:index, :show] do
-    resources 'rentals', only: [:create, :update]
-  end
-  resources 'rentals', only: [:index]
+  resources 'medias', only: [:index, :show]
+  resources 'rentals', only: [:create, :index, :update]
   resources 'artists', only: [:show]
   resources 'users', only: [:show, :edit, :update]
   resources 'categories', only: [:show]
