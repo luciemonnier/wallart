5b8909c365c81a3b123506b568ad9915af846cbe
Leonie Mironesco <leonie.mironesco@hec.edu>
2019-03-14 18:28:12 +0100

    test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    authorize @category
     @medias = Media.where(category: @category)
   end
 end
