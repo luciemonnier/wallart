e24648265f9e9738b01d38bfffbc99769fd1305a
Leonie Mironesco <leonie.mironesco@hec.edu>
2019-03-14 15:55:47 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_143718) do
+ActiveRecord::Schema.define(version: 2019_03_14_145027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_143718) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
   end
 
   create_table "taggings", force: :cascade do |t|
